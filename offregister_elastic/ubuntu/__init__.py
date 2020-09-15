0aaf30e1fef8056c9c3ff713782cada05fc5404d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_elastic/ubuntu/__init__.py
+++ offregister_elastic/ubuntu/__init__.py
@@ -5,23 +5,30 @@ from offregister_fab_utils.ubuntu.systemd import restart_systemd
 
 
 def install0(**kwargs):
-    installed = lambda: run("dpkg-query --showformat='${Version}' --show elasticsearch", quiet=True)
+    installed = lambda: run(
+        "dpkg-query --showformat='${Version}' --show elasticsearch", quiet=True
+    )
 
-    if sudo('dpkg -s elasticsearch', quiet=True, warn_only=True).failed:
-        sudo('wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -')
+    if sudo("dpkg -s elasticsearch", quiet=True, warn_only=True).failed:
+        sudo(
+            "wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -"
+        )
 
-        append('/etc/apt/sources.list.d/elasticsearch.list',
-               'deb http://packages.elasticsearch.org/elasticsearch/{}/debian stable main'.format(
-                   kwargs['VERSION'][:kwargs['VERSION'].rfind('.')] if kwargs['VERSION'].count('.') == 3
-                   else kwargs['VERSION']
-               ),
-               use_sudo=True)
-        sudo('apt update')
-        sudo('apt-get install -y elasticsearch={}'.format(kwargs['VERSION']))
-        if kwargs.get('NO_UPGRADE'):
-            sudo('apt-mark hold elasticsearch')
-        restart_systemd('elasticsearch')
+        append(
+            "/etc/apt/sources.list.d/elasticsearch.list",
+            "deb http://packages.elasticsearch.org/elasticsearch/{}/debian stable main".format(
+                kwargs["VERSION"][: kwargs["VERSION"].rfind(".")]
+                if kwargs["VERSION"].count(".") == 3
+                else kwargs["VERSION"]
+            ),
+            use_sudo=True,
+        )
+        sudo("apt update")
+        sudo("apt-get install -y elasticsearch={}".format(kwargs["VERSION"]))
+        if kwargs.get("NO_UPGRADE"):
+            sudo("apt-mark hold elasticsearch")
+        restart_systemd("elasticsearch")
 
-        return 'elasticsearch {} installed'.format(installed())
+        return "elasticsearch {} installed".format(installed())
 
-    return '[Already] elasticsearch {} installed'.format(installed())
+    return "[Already] elasticsearch {} installed".format(installed())
